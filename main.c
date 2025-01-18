df7c3db5c6fa1c7638540eb42c3ef39c3ac18c0c
J <rippiten23@gmail.com>
2025-01-18 16:13:07 +0300

    http functions added

---
+++ main.c
@@ -0,0 +1,7 @@
+void http_init(){}
+*char http_read_buffer(){}
+*Request http_parse_request(*char buffer){}
+*Response http_response_init(){}
+void http_route_request(){}
+*char http_render_response(*Response response){}
+void http_write_buffer(*char buffer){}
